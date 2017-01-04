8958aa6e3b3758438ebde61ae5c1c2f86cac38d2
Gahoolee <gahoolee@ACA864F7.ipt.aol.com>
2017-01-04 17:22:55 +0800

    add id; add serializerLocal and serializerRedis; add redisProgress; add storeFileInRedis option;

--- R/serializers.R
+++ R/serializers.R
@@ -1,22 +1,50 @@
 # For most types of values, simply return the value unchanged.
-serializerDefault <- function(value, stateDir) {
+serializerDefault <- function(value, stateDir, id) {
   value
 }
 
+readBinaryFile<-function(filepath){
+  size = file.info(filepath)$size
+  readBin(filepath, raw(), size)
+}
 
-serializerFileInput <- function(value, stateDir = NULL) {
+serializerFileInput <- function(value, stateDir = NULL, id = NULL) {
   # File inputs can be serialized only if there's a stateDir
-  if (is.null(stateDir)) {
+  store <- getShinyOption("bookmarkStore", default = "")
+  if (is.null(stateDir) && store != 'redis') {
     return(serializerUnserializable())
   }
 
   # value is a data frame. When persisting files, we need to copy the file to
   # the persistent dir and then strip the original path before saving.
-  newpaths <- file.path(stateDir, basename(value$datapath))
-  file.copy(value$datapath, newpaths, overwrite = TRUE)
-  value$datapath <- basename(newpaths)
+  serializerLocal<-function(stateDir, value){
+    newpaths <- file.path(stateDir, basename(value$datapath))
+    file.copy(value$datapath, newpaths, overwrite = TRUE)
+    value$datapath <- basename(newpaths)
 
-  value
+    value
+  }
+
+  serializerRedis<-function(id, value){
+    saveRedisFile <- function(datapath){
+      filename <- basename(datapath)
+      content <- readBinaryFile(datapath)
+      redisHSet(id, filename, content)
+    }
+    filename <- basename(value$datapath)
+    redisProgress(value$datapath, saveRedisFile, "Save files to redis.")
+    value$datapath <- filename
+
+    value
+  }
+
+  storeFileInRedis <- getShinyOption("storeFileInRedis", default = TRUE)
+  if (store == 'redis' && storeFileInRedis == TRUE) {
+    #redisInterface(id, serializerRedis, value=value)
+    serializerRedis(id, value)
+  } else {
+    serializerLocal(stateDir, value)
+  }
 }
 
 
@@ -43,7 +71,7 @@ isUnserializable <- function(x) {
 # values. This function passes stateDir to the serializer functions, so if
 # stateDir is non-NULL, it can have a side effect of writing values to disk (in
 # stateDir).
-serializeReactiveValues <- function(values, exclude, stateDir = NULL) {
+serializeReactiveValues <- function(values, exclude, stateDir = NULL, id = NULL) {
   impl <- .subset2(values, "impl")
 
   # Get named list where keys and values are the names of inputs; we'll retrieve
@@ -63,7 +91,7 @@ serializeReactiveValues <- function(values, exclude, stateDir = NULL) {
       serializer <- serializerDefault
 
     # Apply serializer function.
-    serializer(val, stateDir)
+    serializer(val, stateDir, id)
   })
 
   # Filter out any values that were marked as unserializable.
