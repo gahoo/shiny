0ea1f4e1151079eab3d37b75c91f73fe34004ec4
Gahoolee <gahoolee@ACA864F7.ipt.aol.com>
2017-01-04 17:12:34 +0800

    add ...; add redisProgress; add redisIndicator; add redisProgress option;

--- R/bookmark-state-redis.R
+++ R/bookmark-state-redis.R
@@ -7,7 +7,7 @@
 # @param callback A callback function that saves state to or restores state from
 #   a directory. It must take one argument, \code{stateDir}, which is a
 #   directory to which it writes/reads.
-redisInterface <- function(id, callback) {
+redisInterface <- function(id, callback, ...) {
   redisConfig <- getShinyOption("redis")
   if(is.null(redisConfig)){
     stop("No redis config.")
@@ -21,6 +21,36 @@ redisInterface <- function(id, callback) {
     )
   }
 
-  callback(id)
+  result <- callback(id, ...)
   redisClose()
+  result
+}
+
+redisProgress <- function(looper, callback, message = "Process redis commands", detail = "Part", ...) {
+  withProgress <- getShinyOption("redisProgress", default = TRUE)
+  if(withProgress){
+    progress <- Progress$new()
+    on.exit(progress$close())
+    progress$set(message = message, value = 0)
+  }
+
+  n <- length(looper)
+  for (i in 1:n) {
+    if(withProgress){
+      progress$inc(1/n, detail = paste(detail, i))
+    }
+    callback(looper[i], ...)
+  }
+}
+
+redisIndicator <- function(callback, message = "Process redis commands", detail = "", ...) {
+  withProgress <- getShinyOption("redisProgress", default = TRUE)
+  if(withProgress){
+    progress <- Progress$new()
+    on.exit(progress$close())
+    progress$set(message = message, value = 0)
+    progress$inc(1, detail = detail)
+  }
+
+  callback(...)
 }
